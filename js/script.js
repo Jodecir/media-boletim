418d6a131538ed763c1ee222f6d6ce5319ef0e8f
Jodecir Neto <jodecirneto@gmail.com>
2021-09-18 09:45:18 -0300

    UPDATE average formula

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ function exe() {
   var n2 = parseInt(document.getElementById("n2").value);
   var n3 = parseInt(document.getElementById("n3").value); 
   var n4 = parseInt(document.getElementById("n4").value);
-  var media = (n1 + n2 + n3 + n4) / 2;
+  var qtdBimestres = 4
+
+  var media = (n1 + n2 + n3 + n4) / qtdBimestres;
   var ok = document.getElementById("ok");
   
   if (media >= 7) {
