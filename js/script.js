659bf63914ff05781ca925d1a60551011a82c854
Jodecir Neto <jodecirneto@gmail.com>
2021-09-27 15:32:49 -0300

    CREATE select box that defines the average

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function exe() {
-  var n1 = parseInt(document.getElementById("n1").value); 
-  var n2 = parseInt(document.getElementById("n2").value);
-  var n3 = parseInt(document.getElementById("n3").value); 
-  var n4 = parseInt(document.getElementById("n4").value);
+  var n1 = parseFloat(document.getElementById("n1").value); 
+  var n2 = parseFloat(document.getElementById("n2").value);
+  var n3 = parseFloat(document.getElementById("n3").value); 
+  var n4 = parseFloat(document.getElementById("n4").value);
+  var valorAprovado = parseInt(document.getElementById("valorAprovado").value)
 
   var result = document.getElementById("result");
   
   var qtdBimestres = 4
   var media = (n1 + n2 + n3 + n4) / qtdBimestres;
-  var valorAprovado = 7
   var notaFixada = media.toFixed(1);
   var mediaFaltante = valorAprovado - notaFixada;
   var notaFaltante = mediaFaltante.toFixed(1) * qtdBimestres;
